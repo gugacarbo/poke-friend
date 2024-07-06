d59845d3abd6ff676f967fa7c0f402e1a7560126
Gustavo Carbonera <guga_carbo@hotmail.com>
2024-07-06 19:14:12 -0300

    reuntime edge exports on non-static route [locale]

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@/features/Layout";
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <main className="flex flex-col justify-between items-center py-6 min-h-screen">
